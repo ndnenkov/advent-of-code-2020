854d7f1bb102432b4d0cf47d6c89bd399e055c65
Nikola Nenkov <ndnenkov@gmail.com>
2020-12-23 20:54:28 +0200

    Simplify 15

--- 15/a.rb
+++ 15/a.rb
@@ -6,31 +6,19 @@ TEXT
 # 0,3,6
 # TEXT
 
-NUM_AGES = (NUMBERS - [NUMBERS.last]).each_with_index.map {|x, i| [x, i+1]}.to_h
+ages = NUMBERS[0...-1].map.with_index { |number, index| [number, index.next] }.to_h
 
-turn = NUMBERS.size - 1# - 1
+turn = NUMBERS.size
 last = NUMBERS.last
 
-puts NUM_AGES.keys
-puts '=========='
 loop do
-  # puts "#{turn}: #{last} (#{NUM_AGES})"
-  # break if turn == 2020
-  break if turn == 2019
-  # break if turn == 10
-  turn += 1
+  break if turn == 2020
 
-  x = NUM_AGES[last]
-  NUM_AGES[last] = turn
-  if x
-    # puts "(#{turn - x}) [#{NUM_AGES}] found #{last} = #{x}, setting last = #{turn} - #{x}"
-    last = turn - x# - 1
-  else
-    # puts "(0) [#{NUM_AGES}] not found #{last} = #{x}, setting last = 0"
-    last = 0
-  end
-  # puts last
-  # puts '-------------' if last == 436
+  last_age = ages[last]
+  ages[last] = turn
+  last = last_age ? turn - last_age : 0
+
+  turn += 1
 end
 
 puts last
