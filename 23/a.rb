f2c2e4e041785eab6071db4d672fc91f0125d66d
Nikola Nenkov <ndnenkov@gmail.com>
2020-12-23 18:03:46 +0200

    Use b solution for a part instead of complicated a solution

--- 23/a.rb
+++ 23/a.rb
@@ -2,61 +2,36 @@ INPUT = '219748365'
 
 # INPUT = '389125467'
 
-cups = INPUT.chars.map(&:to_i)
-current_cup = cups.first
+cups = INPUT.chars.map(&:to_i).each_cons(2).map do |first, second|
+  [first, {number: first, next_number: second}]
+end.to_h
+current_cup = cups.first.last
+cups[INPUT[-1].to_i] = {number: INPUT[-1].to_i, next_number: current_cup[:number]}
 
-cups_slice = lambda do |from_index|
-  min_index = from_index.next % cups.size
-  max_index = (from_index + 3) % cups.size
+100.times do
+  first_cup = cups[current_cup[:next_number]]
+  second_cup = cups[first_cup[:next_number]]
+  third_cup = cups[second_cup[:next_number]]
 
-  if min_index > max_index
-    cups[min_index..-1] + cups[0..max_index]
-  else
-    cups[min_index..max_index]
-  end
-end
+  current_cup[:next_number] = third_cup[:next_number]
 
-step = 1
-loop do
-  break if step == 101
-
-  current_index = cups.index(current_cup)
-  aside_cups = cups_slice.(current_index)
-  cups_in_play = cups - aside_cups
-
-  search_from = (current_cup.pred.zero? || ((1..current_cup.pred).to_a - aside_cups).empty?) ? 9 : current_cup.pred
-  destination_cup = (9.downto(1).to_a - aside_cups).find { |digit| digit <= search_from }
-  destination_cup_index = cups_in_play.index(destination_cup)
-
-  cups = cups_in_play.insert destination_cup_index.next, *aside_cups
-
-  cups_cycle = cups.cycle
-
-  new_cups = cups
-  loop do
-    new_cups = cups.size.times.map { cups_cycle.next }
-    if new_cups.index(current_cup) == current_index
-      break
+  destination_number =
+    current_cup[:number].pred.downto(1).chain(9.downto(current_cup[:number])).find do |number|
+      [first_cup, second_cup, third_cup].none? { |cup| cup[:number] == number }
     end
 
-    cups_cycle.next
-  end
+  destination_cup = cups[destination_number]
+  third_cup[:next_number] = destination_cup[:next_number]
+  destination_cup[:next_number] = first_cup[:number]
 
-  cups = new_cups
-
-  current_cup = cups[current_index.next % cups.size]
-  step += 1
+  current_cup = cups[current_cup[:next_number]]
 end
 
-cups_cycle = cups.cycle
-without_one = cups
-loop do
-  without_one = cups.size.times.map { cups_cycle.next }
-  if without_one.index(1).zero?
-    break
-  end
-
-  cups_cycle.next
+cups_after_one = ''
+current_cup = cups[1]
+8.times do
+  current_cup = cups[current_cup[:next_number]]
+  cups_after_one += current_cup[:number].to_s
 end
 
-puts without_one[1..-1].join
+puts cups_after_one
