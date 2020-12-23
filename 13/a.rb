a7e0ef797dcd642bea83c6cec9136e33d86b7469
Nikola Nenkov <ndnenkov@gmail.com>
2020-12-23 20:02:04 +0200

    Cleanup 13

--- 13/a.rb
+++ 13/a.rb
@@ -9,44 +9,17 @@ TEXT
 # TEXT
 
 timestamp = NOTES.first.to_i
-bus_numbers = NOTES.last.split(',').reject {|x| x == 'x'}.map(&:to_i)
+busses = NOTES.last.split(',').difference(%w(x)).map(&:to_i)
 
-# p bus_numbers
-# bus =
-# bus_numbers.min do |num|
-#   puts "#{num} : #{(timestamp - (timestamp / num + 1) * num).abs}"
-#   (timestamp - (timestamp / num + 1) * num).abs
-#   # res = timestamp / num
-#   # puts res
-#   # if res * num == timestamp
-#   #   timestamp
-#   # else
-#   #   (res + 1) * num
-#   # end
-# end
+earliest_bus = nil
+earliest_bus_wait_time = Float::INFINITY
+busses.each do |bus|
+  wait_time = (timestamp - (timestamp / bus + 1) * bus).abs
 
-buzz = 0
-rezz = 210210200210210120210021021012012
-bus_numbers.each do |num|
-  # puts "#{num} : #{(timestamp - (timestamp / num + 1) * num).abs}"
-  res = (timestamp - (timestamp / num + 1) * num).abs
-  if rezz > res
-    buzz = num
-    rezz = res
+  if earliest_bus_wait_time > wait_time
+    earliest_bus = bus
+    earliest_bus_wait_time = wait_time
   end
-  # res = timestamp / num
-  # puts res
-  # if res * num == timestamp
-  #   timestamp
-  # else
-  #   (res + 1) * num
-  # end
 end
-p buzz
-bus = buzz
 
-wait_time = (timestamp - (timestamp / bus + 1) * bus).abs
-puts wait_time
-
-# p "#{bus} * (#{wait_time} - #{timestamp})"
-puts bus * wait_time
+puts earliest_bus * earliest_bus_wait_time
--- 13/b.rb
+++ 13/b.rb
@@ -8,47 +8,23 @@ TEXT
 # 7,13,x,x,59,x,31,19
 # TEXT
 
-bus_numbers = NOTES.last.split(',').map(&:to_i)
+busses = NOTES.last.split(',').map(&:to_i)
 
-bus_numbers.each_with_index do |number, index|
-  puts "(x + #{index}) = 0 (mod #{number})" if number.positive?
+busses.each_with_index do |bus, index|
+  puts "x ≡ #{(bus - index) % bus} (mod #{bus})" if bus.positive?
 end
 
-puts '=================='
-
-# (x + 0) = 0 (mod 29)
-# (x + 19) = 0 (mod 41)
-# (x + 29) = 0 (mod 661)
-# (x + 42) = 0 (mod 13)
-# (x + 43) = 0 (mod 17)
-# (x + 52) = 0 (mod 23)
-# (x + 60) = 0 (mod 521)
-# (x + 66) = 0 (mod 37)
-# (x + 79) = 0 (mod 19)
-
-bus_numbers.each_with_index do |number, index|
-  puts "x = #{(number - index) % number} (mod #{number})" if number.positive?
-end
-
-# x = 0 (mod 29)
-# x = 22 (mod 41)
-# x = 632 (mod 661)
-# x = 10 (mod 13)
-# x = 8 (mod 17)
-# x = 17 (mod 23)
-# x = 461 (mod 521)
-# x = 8 (mod 37)
-# x = 16 (mod 19)
+# x ≡ 0 (mod 29)
+# x ≡ 22 (mod 41)
+# x ≡ 632 (mod 661)
+# x ≡ 10 (mod 13)
+# x ≡ 8 (mod 17)
+# x ≡ 17 (mod 23)
+# x ≡ 461 (mod 521)
+# x ≡ 8 (mod 37)
+# x ≡ 16 (mod 19)
 
 # https://en.wikipedia.org/wiki/Chinese_remainder_theorem
 # https://www.dcode.fr/chinese-remainder
-# x ≡ 0 mod 29
-# x ≡ 22 mod 41
-# x ≡ 632 mod 661
-# x ≡ 10 mod 13
-# x ≡ 8 mod 17
-# x ≡ 17 mod 23
-# x ≡ 461 mod 521
-# x ≡ 8 mod 37
-# x ≡ 16 mod 19
+
 # x = 213890632230818
