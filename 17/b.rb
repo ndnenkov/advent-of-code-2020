3ec674b4b657f88a5fc2937b4027f8201e30a60d
Nikola Nenkov <ndnenkov@gmail.com>
2020-12-25 14:09:34 +0200

    Cleanup 17

--- 17/b.rb
+++ 17/b.rb
@@ -1,4 +1,4 @@
-INIT_STATE = <<~TEXT.split("\n").map(&:chars)
+INITIAL_STATE = <<~TEXT.split("\n").map(&:chars)
 ....###.
 #...####
 ##.#.###
@@ -9,43 +9,46 @@ INIT_STATE = <<~TEXT.split("\n").map(&:chars)
 ######.#
 TEXT
 
-# INIT_STATE = <<~TEXT.split("\n").map(&:chars)
+# INITIAL_STATE = <<~TEXT.split("\n").map(&:chars)
 # .#.
 # ..#
 # ###
 # TEXT
 
-STATE = {}
+state = {}
 
-INIT_STATE.each_with_index do |row, rindex|
-  row.each_with_index do |cell, cindex|
-    STATE[[rindex, cindex, 0, 0]] = cell == '#'
+INITIAL_STATE.each_with_index do |row, row_index|
+  row.each_with_index do |cell, column_index|
+    state[[row_index, column_index, 0, 0]] = cell == '#'
   end
 end
 
-def tick(state)
+def cycle(state)
+  (min_x, max_x), (min_y, max_y), (min_z, max_z), (min_w, max_w) =
+    state.keys.transpose.map(&:minmax).map { |min, max| [min.pred, max.next] }
+
   counts = {}
-  ((minx, maxx), (miny, maxy), (minz, maxz), (minw, maxw)) = state.keys.transpose.map(&:minmax)
-  minx -= 1
-  miny -= 1
-  minz -= 1
-  minw -= 1
-  maxx += 1
-  maxy += 1
-  maxz += 1
-  maxw += 1
+  (min_x..max_x).to_a.product(
+    (min_y..max_y).to_a,
+    (min_z..max_z).to_a,
+    (min_w..max_w).to_a,
+  ).each do |x, y, z, w|
+    [-1, 0, 1].product(
+      [-1, 0, 1],
+      [-1, 0, 1],
+      [-1, 0, 1],
+    ).each do |x_offset, y_offset, z_offset, w_offset|
+      next if x_offset.zero? && y_offset.zero? && z_offset.zero? && w_offset.zero?
 
-  (minx..maxx).to_a.product((miny..maxy).to_a, (minz..maxz).to_a, (minw..maxw).to_a).each do |x, y, z, w|
-    [-1, 0, 1].product([-1, 0, 1], [-1, 0, 1], [-1, 0, 1]).each do |xoff, yoff, zoff, woff|
-      next if xoff.zero? && yoff.zero? && zoff.zero? && woff.zero?
-
-      counts[[x,y,z,w]] ||= 0
-      counts[[x,y,z,w]] += 1 if state[[x+xoff, y+yoff, z+zoff,w+woff]]
+      counts[[x, y, z, w]] ||= 0
+      if state[[x + x_offset, y + y_offset, z + z_offset, w + w_offset]]
+        counts[[x, y, z, w]] += 1
+      end
     end
   end
 
   counts.each do |key, count|
-    if state[key] && [2,3].include?(count)
+    if state[key] && [2, 3].include?(count)
       counts[key] = true
     elsif count == 3
       counts[key] = true
@@ -57,12 +60,6 @@ def tick(state)
   counts
 end
 
-6.times do
-  STATE = tick STATE
-end
+6.times { state = cycle state }
 
-puts STATE.values.count(&:itself)
-
-
-
-
+puts state.values.count(&:itself)
